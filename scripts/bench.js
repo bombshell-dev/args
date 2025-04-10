9289c9725a5ac065521b29a87ad6b92ea4191f22
Jan Fooken <jan@faulty.computer>
2025-04-10 13:34:09 +0200

    Fix Spacing and Library Name in bench.js
    The spacing in scripts/bench.js was off, leading to an unconsitent
    visual output when running the benchmark.  Furthermore, the benchmark
    was still named ultraflags.

--- scripts/bench.js
+++ scripts/bench.js
@@ -9,10 +9,10 @@ const bench = new benchmark.Suite();
 const args = ['--a=1', '-b', '--bool', '--no-boop', '--multi=foo', '--multi=baz', '-xyz'];
 
 bench
-  .add('ultraflag    ', () => ultraflag(args))
-  .add('mri          ', () => mri(args))
-  .add('minimist     ', () => minimist(args))
-  .add('node:util', () => parseArgs({ args, strict: false, allowNegative: true, options: { bool: { type: 'boolean', short: 'b' }, boop: { type: 'boolean' }, multi: { type: 'string' }} }))
-  .add('yargs-parser ', () => yargs(args))
+  .add('args        ', () => ultraflag(args))
+  .add('minimist    ', () => minimist(args))
+  .add('mri         ', () => mri(args))
+  .add('node:util   ', () => parseArgs({ args, strict: false, allowNegative: true, options: { bool: { type: 'boolean', short: 'b' }, boop: { type: 'boolean' }, multi: { type: 'string' }} }))
+  .add('yargs-parser', () => yargs(args))
   .on('cycle', e => console.log(String(e.target)))
   .run();
